35c798823e59bfca9c3ba948b39df12f7ccf66a1
Marcio <marcio@draimo.com>
2020-10-04 14:04:12 -0300

    Updates

---
+++ precache-manifest.df6ce0bedadf0f996ceba3fbeb3c017f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d435041240cfa01bcecf136ca1937bb",
+    "url": "/REACT-movie-ratings/index.html"
+  },
+  {
+    "revision": "8e158f66620ed47edf8e",
+    "url": "/REACT-movie-ratings/static/css/main.71d1980e.chunk.css"
+  },
+  {
+    "revision": "690f527c15ceeabac2f9",
+    "url": "/REACT-movie-ratings/static/js/2.3f9b5313.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/REACT-movie-ratings/static/js/2.3f9b5313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e158f66620ed47edf8e",
+    "url": "/REACT-movie-ratings/static/js/main.99d1f13a.chunk.js"
+  },
+  {
+    "revision": "e384393050b791751b39",
+    "url": "/REACT-movie-ratings/static/js/runtime-main.70897cc7.js"
+  },
+  {
+    "revision": "2df31fb3155a3303e55e442959cb5381",
+    "url": "/REACT-movie-ratings/static/media/error-404.2df31fb3.png"
+  },
+  {
+    "revision": "5405d77c51fb46a0cbf26cb96fe4da4d",
+    "url": "/REACT-movie-ratings/static/media/no-img.5405d77c.png"
+  }
+]);
--- src/utils/getMovieById.tsx
+++
@@ -1,7 +0,0 @@
-import movies from '../data/movies.json'
-
-export const getMovieById = (movieId: any) => {
-    return movies.find(movie => movie.pk === parseInt(movieId));
-}
-
-export default getMovieById
